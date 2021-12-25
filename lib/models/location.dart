1680e7532b4f1537b8a17a511d795eb5876e3508
harsh <harshchauhan5180@gmail.com>
2021-12-25 16:30:57 +0530

    Added json serializable package.

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,5 +1,23 @@
-enum LocationType { city, region, state, province, country, continent }
+import 'package:json_annotation/json_annotation.dart';
+import '../enums/enums.dart';
+part 'location.g.dart';
 
+enum LocationType {
+  @JsonValue('City')
+  city,
+  @JsonValue('Region')
+  region,
+  @JsonValue('State')
+  state,
+  @JsonValue('Province')
+  province,
+  @JsonValue('Country')
+  country,
+  @JsonValue('Continent')
+  continent
+}
+
+@JsonSerializable()
 class Location {
   const Location({
     required this.title,
@@ -8,8 +26,13 @@ class Location {
     required this.woeid,
   });
 
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
   final String title;
   final LocationType locationType;
+  @JsonKey(name: 'latt_long')
+  @LatLngConverter()
   final LatLng latLng;
   final int woeid;
 }
@@ -19,4 +42,22 @@ class LatLng {
 
   final double latitude;
   final double longitude;
+}
+
+class LatLngConverter implements JsonConverter<LatLng, String> {
+  const LatLngConverter();
+
+  @override
+  String toJson(LatLng latLng) {
+    return '${latLng.latitude},${latLng.longitude}';
+  }
+
+  @override
+  LatLng fromJson(String jsonString) {
+    final parts = jsonString.split(',');
+    return LatLng(
+      latitude: double.tryParse(parts[0]) ?? 0,
+      longitude: double.tryParse(parts[1]) ?? 0,
+    );
+  }
 }
